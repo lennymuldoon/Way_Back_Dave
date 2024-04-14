a7f1ab4ca58719ac0e109012f96dbcfbf46e2145
lennymuldoon <lenny.muldoon@gmail.com>
2024-04-14 07:22:31 -0500

    Running animation for dave
    made running and idle animation

--- Way_Back_Dave/Assets/Scripts/PlayerMovement.cs
+++ Way_Back_Dave/Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+    Vector2 respawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        respawnPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
